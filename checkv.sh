9584e9ca5a2d9c7220808ec444e6467faff19f50
Emilie J. Skoog <48771085+emilieskoog@users.noreply.github.com>
2025-04-08 09:19:34 -0700

    Create checkv.sh

---
+++ checkv.sh
@@ -0,0 +1,3 @@
+#Performing viral qualify filtering after merging all viral contigs together
+
+checkv end_to_end botswana_all_viral_contigs.fa checkv_botswana_all_viral_contigs_output_directory -t 32
